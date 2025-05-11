b82d09bac18fbdea2677d6476c882f3df7f6e811
kateyatsk <klaettoe2023@gmail.com>
2025-05-11 22:00:47 +0300

    Initial Commit

---
+++ Layout2.0/Layout2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Layout2_0App.swift
+//  Layout2.0
+//
+//  Created by Екатерина Яцкевич on 11.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Layout2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
